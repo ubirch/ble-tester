812b4c7f385c0c58abe2fb0f5c71b1f9dc96a2ba
Niranjan Rao <niranjanhrao@gmail.com>
2017-07-26 14:09:56 +0200

    unpack and print msgpack trackle sensor values

--- trackle_ble_client.py
+++ trackle_ble_client.py
@@ -1,3 +1,5 @@
+# !/usr/bin/env python
+
 import struct
 import sys
 
@@ -6,21 +8,31 @@ import time
 from bluepy.btle import UUID, Peripheral, DefaultDelegate, Scanner
 
 from StringIO import StringIO
+import socket
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = "Hello, World!"
+
 mydata = StringIO()
 
 def unpack_msgpack():
-    print "unpack"
+    print "Unpack Temperature Values..."
     thestr =  StringIO(mydata.getvalue())
-    # print thestr[]
-    unpacker = msgpack.Unpacker(mydata)
-    print unpacker
+    unpacker = msgpack.Unpacker(thestr)
+
     for val in unpacker:
         print val
-        print len(val)
+    print "All the sensor values unpacked..."
+def send_data_to_backend():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+    s.send(mydata.getvalue())
+    data = s.recv(BUFFER_SIZE)
+    s.close()
+    print "received data:", data
 
-    print "unpacked"
-
-# readData = 0
 class MyDelegate(DefaultDelegate):
     readData = False
     #Constructor (run once on startup)
@@ -31,27 +43,24 @@ class MyDelegate(DefaultDelegate):
         # print ("Notification from Handle: 0x" + format(cHandle,'02X') + " Value: "+ format(ord(data[0])))
         print data
         if 'No more data' in data:
-            # x = ''
-            # y = x.replace('NONE\r\n','')
-            # for x in data:
-            # mydata.write(y)
-
-            print "No data..."
+            print "No data tp send..."
             self.readData = True
-            # unpack_msgpack()
-            # print ""
+
+        if 'OK let me sleep...' in data:
+            print "Trackle is going to sleep..."
+
+        elif "\r\n\r\n" in data:
+            newData = data[:-4]
+            mydata.write(newData)
+            print newData
 
         else:
-            # self.readData = False
             mydata.write(data)
 
     def getTxflag(self):
         return self.readData
 
-
-
-
-
+# ++++++++++++++++++++++++++++++ BLE START ++++++++++++++++++++++++++++
 # Trackle Service and Char UUIDs
 UART_service_uuid    = UUID("6e400001-b5a3-f393-e0a9-e50e24dcca9e")
 UART_rx_char_uuid    = UUID("6e400003-b5a3-f393-e0a9-e50e24dcca9e")
@@ -71,16 +80,16 @@ for dev in devices:
     for (adtype, desc, value) in dev.getScanData():
         print "  %s = %s" % (desc, value)
 
-deviceIndex = 1
-# deviceIndex = int(input("Input the device Index:"))
-# if 0 > deviceIndex > 10:
+# deviceIndex = 1
+deviceIndex = int(input("Input the device Index:"))
 
-print deviceIndex
-# print scanDeviceList[deviceIndex]
+# print deviceIndex
+print scanDeviceList[deviceIndex]
 
-trackleBLE = "c2:bf:e4:96:9e:4b"
-# p = Peripheral(scanDeviceList[deviceIndex], "random")
-p = Peripheral(trackleBLE, "random")
+# trackleBLE = "c2:bf:e4:96:9e:4b"
+# trackleBLE  = "e0:87:40:09:11:ea"
+p = Peripheral(scanDeviceList[deviceIndex], "random")
+# p = Peripheral(trackleBLE, "random")
 pq = MyDelegate(p)
 p.setDelegate(pq)
 # p.setDelegate( MyDelegate(p))
@@ -95,16 +104,16 @@ hButtonC=ButtonC.getHandle()
 #  wich is located in a handle in the range defined by the boundries of the ButtonService
 for desriptor in p.getDescriptors(hButtonC):  # The handle range should be read from the services
    if (desriptor.uuid == 0x2902):                   #      but is not done due to a Bluez/BluePy bug :(     
-        print ("Button1 Client Characteristic Configuration found at handle 0x"+ format(desriptor.handle,"02X"))
+        print ("Trackle UART Client Characteristic Configuration found at handle 0x"+ format(desriptor.handle,"02X"))
         hButtonCCC=desriptor.handle
  
 # Turn notifications on by setting bit0 in the CCC more info on:
 # https://developer.bluetooth.org/gatt/descriptors/Pages/DescriptorViewer.aspx?u=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml    
 p.writeCharacteristic(hButtonCCC, struct.pack('<bb', 0x01, 0x00))
-print "Notification is turned on for Button1"
+print "Notification is turned on for Trackle sensor"
 
+# ++++++++++++++++++++++++++++++ BLE STOP  ++++++++++++++++++++++++++++
 
-# Main loop --------
 while True:
     if p.waitForNotifications(1.0):
         # handleNotification() was called
@@ -112,12 +121,9 @@ while True:
 
     if pq.getTxflag() is True:
         time.sleep(0.5)
-        print "readData"
+        print "Read the Trackle data..."
         pq.readData = False
         unpack_msgpack()
     else:
-        print "Waiting... Waited more than one sec for notification"
-
-    # print readData
-        # print mydata.getvaue()
-        # Perhaps do something else here
+        print '...'
+        # print "Waiting... Waited more than one sec for notification"
